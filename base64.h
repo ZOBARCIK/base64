da9c89b0ef50e50e71911cddd51cf176d764db91
Ahmet Arda ZOBAR <ardazobar@outlook.com>
2024-09-14 23:32:35 +0300

    it can encode EUNJUNG now, yaay

--- base64.h
+++ base64.h
@@ -5,10 +5,14 @@
 //append each chunk character to output string
 //if number of bits in binary data is not a multiple of 3, pad with = sign
 //handle data 3bytes at a time, 24 is EKOK of 6 and 8
+//build decoding table when needed in order to save some ROM on the controller
 
 //todo;
 //catching nulls
 //back propogation
+//decoding
+
+//NOTE: decoding is incomplete(while loop)
 
 #ifndef BASE64_H
 #define BASE64_H
@@ -17,8 +21,8 @@
 #include <stdlib.h>
 
 
-char *encode64(const unsigned char *input, uint32_t input_length);
-
+unsigned char *encode64(const unsigned char *input, uint32_t input_length);
+unsigned char *build_decodding_table();
 
 //array width 65 maybe?
 static char base64_table[65] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',  
@@ -34,26 +38,25 @@ static char *decoding_table = NULL;
 static int mod[4] = {0, 2, 1}; //for padding 
 
 
-char *encode64(const unsigned char *input, uint32_t input_length){
+unsigned char *encode64(const unsigned char *input, uint32_t input_length){
 
 
 
 
-    uint32_t encoded_length = 4*(input_length + 2)/3; //create 4 byte chunk of output
+    unsigned char encoded_length = 4*(input_length + 2)/3; //create 4 byte chunk of output
 
     if (input_length>encoded_length) {
         return NULL; //input overflow, try catching this in future
     }
 
-    char *encoded_data = malloc(encoded_length+1);
-    if (encoded_data == NULL) {
+    char *encoded_out = malloc(encoded_length+1);
+    if (encoded_out == NULL) {
         return NULL; //try catching this
     }
 
     //poutition indices
-    unsigned char *pout=encoded_data;
+    unsigned char *pout=encoded_out;
     const unsigned char *pin= input;
-    const unsigned char *pend = input + input_length;
 
     //             pin[0]                           pin[1]                          pin[2]
     // input b7 b6 b5 b4 b3 b2 b1 b0 - b15 b14 b13 b12 b11 b10 b9 b8 - b23 b22 b21 b20 b19 b18 b17 b16  
@@ -62,7 +65,7 @@ char *encode64(const unsigned char *input, uint32_t input_length){
 
     uint32_t i=0;
     while(input_length>=3){     // three bytes at a time
-        *pout++= base64_table[pin[i] >> 2]; // shift right 2 bits b7 0 0 b6 b5 b4 b3 b2   
+        *pout++= base64_table[pin[i] >> 2]; // shift right 2 bits 0 0 b7 b6 b5 b4 b3 b2   
         *pout++= base64_table[((pin[i] & 0x03) << 4) | (pin[i+1] >> 4)]; //  b1 b0 0 0 0 0 0 0 | 0 0 0 0 b15 b14 b13 b12 == b1 b0 b15 b14 b13 b12 0 0
         *pout++= base64_table[((pin[i+1] & 0x0f) << 2) | (pin[i+2] >> 6)]; //  b11 b10 b9 b8 0 0 0 0 | 0 0 0 0 0 0 b23 b22 == b11 b10 b9 b8 b23 b22 0 0
         *pout++=base64_table[pin[2] & 0x03f]; // b21 b20 b19 b18 b17 b16 0 0
@@ -72,7 +75,7 @@ char *encode64(const unsigned char *input, uint32_t input_length){
 
     if(input_length!=0){
         *pout++=base64_table[pin[i]>>2];
-        if(pend-pin==1){
+        if(input_length==1){
             *pout++=base64_table[(pin[i] & 0x03) << 4];
             *pout++='=';
         }else{
@@ -82,7 +85,76 @@ char *encode64(const unsigned char *input, uint32_t input_length){
         *pout++='=';
     }
     *pout = '\0';
-    return encoded_data;
+    return encoded_out;
+}
+
+unsigned char *decode64(const unsigned char *input, uint32_t input_length){
+
+    if (decoding_table == NULL) {
+        decoding_table=build_decodding_table(); //try catching the null
+    }
+
+    if (input_length % 4 != 0) {
+        return NULL; //try catching this
+    }
+
+    decoded_length = (input_length/4)*3;
+
+    //remove paddings
+    if (data[input_length - 1] == '='){(decoded_length)--;
+    if (data[input_length - 2] == '='){(decoded_length)--;
+
+    unsigned char *decoded_out = malloc(decoded_length+1);
+    if(decoded_out == NULL){
+        return NULL; //try catching this
+    }
+
+
+    const unsigned char *pin= input;
+    unsigned char *pout=decoded_out;
+
+    //             pin[0]                           pin[1]                          pin[2]
+    // input b7 b6 b5 b4 b3 b2 b1 b0 - b15 b14 b13 b12 b11 b10 b9 b8 - b23 b22 b21 b20 b19 b18 b17 b16  
+
+    //              pin[0]                    pin[1]                       pin[2]                      pin[3]
+    // input 0 0 b7 b6 b5 b4 b3 b2 - b1 b0 b15 b14 b13 b12 0 0 - b11 b10 b9 b8 b23 b22 0 0 -b21 b20 b19 b18 b17 b16 0 0
+
+    //incomplete
+    while (input_length!=0){
+    
+    *pout++= base64_table[pin[i] | (pin[i+1] & 0x03)]; //0 0 b7 b6 b5 b4 b3 b2 | b1 b0 0 0 0 0 0 0 = b7 b6 b5 b4 b3 b2 b1 b0
+
+
+    i+=4;
+    }
+//        *pout++= base64_table[pin[i] >> 2]; // shift right 2 bits 0 0 b7 b6 b5 b4 b3 b2   
+//       *pout++= base64_table[((pin[i] & 0x03) << 4) | (pin[i+1] >> 4)]; //  b1 b0 0 0 0 0 0 0 | 0 0 0 0 b15 b14 b13 b12 == b1 b0 b15 b14 b13 b12 0 0
+//       *pout++= base64_table[((pin[i+1] & 0x0f) << 2) | (pin[i+2] >> 6)]; //  b11 b10 b9 b8 0 0 0 0 | 0 0 0 0 0 0 b23 b22 == b11 b10 b9 b8 b23 b22 0 0
+//        *pout++=base64_table[pin[2] & 0x03f]; // b21 b20 b19 b18 b17 b16 0 0
+//        i+=3;
+
+        input_length-=4;
+    }
+}
+
+
+unsigned char *build_decodding_table(){
+
+    decoding_table=malloc(256);
+    if (decoding_table == NULL) {
+        return NULL; //try catching this
+    }
+
+    uint32_t i=0;
+    while(i<=64){
+        decoding_table[(unsigned char) base64_table[i]]=i;
+        i++;
+    }
+    return decoding_table;
+}
+
+void clear_decoding_table(unsigned char *decoding_table){
+    free(decoding_table);
 }
 
 #endif // BASE64_H
--- readme.md
+++ readme.md
@@ -3,9 +3,14 @@
 currently only encoder
 works somehow
 
-todo:
-decoding
-catching nulls
-back propogation
-c++
+changes:\
+fixed a bug padding where it would interpret the first padding as base64 A(zeroes)/
+started decoding part/
+
+
+todo: \
+complete decoding \
+catching nulls \
+back propogation \
+c++ \
  
